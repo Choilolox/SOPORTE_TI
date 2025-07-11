888f1a9e2a5657982ba5420fcd1b8aaf99dad876
Roberto Plancarte <perezplancarter@gmail.com>
2025-07-11 17:21:34 -0600

    Update Dependencias.kt

--- Dependencias.kt
+++ Dependencias.kt
@@ -1,8 +1,4 @@
-// Proyecto: App de Soporte Técnico Empresarial
-// Lenguaje: Kotlin (Android Studio)
-// Arquitectura: MVVM + Room + Retrofit + Firebase Notificaciones
 
-// ------------------ build.gradle (Module: app) ------------------
 dependencies {
     implementation "androidx.core:core-ktx:1.12.0"
     implementation "androidx.appcompat:appcompat:1.6.1"
